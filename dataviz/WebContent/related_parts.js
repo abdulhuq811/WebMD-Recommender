6750fde945c65f1aa99035f38335614f503d10b2
RangarajKaushikSundar <kaushik.rangaraj@gmail.com>
2016-12-04 23:54:45 -0700

    feat(serverCode): Server Code and DB

---
+++ dataviz/WebContent/related_parts.js
@@ -0,0 +1,24 @@
+var description = {
+	"male":{
+			"leg":["Ankle","Knee","Ligament","Limb","Lower Leg","Tissue","Nails","Toe","Feet","Fingers","Heel","Bones"],
+			"chest":["Diaphragm","Bowel","Esophagus","Heart","Lungs","Bones"],  
+			"arms":["Palms","Arm Pits","Elbow","Nails","Shoulders","Bones","Wrist"], 
+			"pelvis":["Pelvis","Penis","Prostate","Urinary Bladder","Vasectomy","Urinary Tract","Reproductive","Scrotum","Testicles","Bones"],
+			"abdomen":["Biliary Tract","Torso","Bladder","Duct","Gallbladder","Gastrointestinal tract","Hip","Intestine","Kidneys","Liver","Pancreas","Stomach","Navel","Digestive System"],
+			"neck":["Shoulders","Throat","Thyroid","Voice Box"],
+			"head":["Brain","Jaw","Ears","Eyes","Face","Forehead","Hair","Head","Mouth","Nose","Teeth","Headache","Temple","Tongue"],
+			"back":["Backbone","Lower Back","Spine","Bones"],
+			"buttock":["Anal","Anus","Appendix","Rectum","Bones"]
+		},
+	"female":{
+			"leg":["Ankle","Knee","Ligament","Limb","Lower Leg","Tissue","Nails","Toe","Feet","Fingers","Heel","Bones"],
+			"chest":["Breast","Diaphragm","Bowel","Esophagus","Heart","Lungs","Bones"],  
+			"arms":["Arm Pits","Palms","Elbow","Nails","Shoulders","Bones","Wrist"], 
+			"pelvis":["Pelvis","Reproductive","Cervix","Vulva","Urinary Bladder","Urinary Tract","Ovaries","Uterus","Vagina","Bones"],
+			"abdomen":["Biliary Tract","Torso","Torso","Navel","Bladder","Duct","Gallbladder","Gastrointestinal tract","Hip","Intestine","Kidneys","Liver","Pancreas","Stomach","Navel","Digestive System","Fallopian Tube"],
+			"neck":["Shoulders","Throat","Thyroid","Voice Box"],
+			"head":["Brain","Jaw","Ears","Eyes","Face","Forehead","Hair","Head","Mouth","Nose","Teeth","Temple","Tongue"],
+			"back":["Backbone","Lower Back","Spine","Bones"],
+			"buttock":["Anal","Anus","Appendix","Rectum","Bones"]
+	}
+}
